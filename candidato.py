2c0b7ce40f5619d2d876d9758c4de831ef4cec41
KarloRomero <karlorc1598@gmail.com>
2023-07-01 08:47:44 -0500

    "Actualizacion"

--- candidato.py
+++
@@ -1,5 +0,0 @@
-class Candidato:
-    def __init__(self, id, nombre, partido):
-        self.id = id
-        self.nombre = nombre
-        self.partido = partido
--- votante.py
+++
@@ -1,6 +0,0 @@
-class Votante:
-    def __init__(self, id, dni, username, password):
-        self.id = id
-        self.dni = dni
-        self.username = username
-        self.password = password
