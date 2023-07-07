545c9524c3d11f09cbfbf9fc943426a8b42aa993
ElvisMan2 <104602581+ElvisMan2@users.noreply.github.com>
2023-07-07 10:12:00 -0500

    eliminando todo xD

--- PruebaMain/result_presenter.py
+++
@@ -1,8 +0,0 @@
-class ResultPresenter:
-    @staticmethod
-    def present_results(rango_edad_min, rango_edad_max, candidato_mas_votos, cantidad_votos, detalles_votos):
-        print(f"Resultados para el rango de edad {rango_edad_min}-{rango_edad_max}:")
-        print(f"El candidato con más votos es {candidato_mas_votos} con {cantidad_votos} votos.")
-        print("Detalles de los votos:")
-        for votante in detalles_votos:
-            print(f"DNI: {votante.dni}, Nombre: {votante.nombre}, Apellido: {votante.apellido}, Región: {votante.region}")
--- main_user.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append("./vista") 
-from login import Login
-
-ventana_app = Login()
-ventana_app.iniciar()
