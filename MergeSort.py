707e49dbe34c53a2a3bb8aae1347aa02e6da25db
UserNanno <juan.canecillas@unmsm.edu.pe>
2023-07-07 03:24:52 -0500

    Añadiendo pruebaIntegración

--- MergeSort.py
+++ MergeSort.py
@@ -19,7 +19,7 @@ def merge(left, right):
     i = j = 0
 
     while i < len(left) and j < len(right):
-        if left[i][1] > right[j][1]:
+        if left[i][1] < right[j][1]: # Ordenar por la cantidad de votos: > paa que muestre el que tiene menos votos y < para que muestre el que tiene mas votos
             result.append(left[i])
             i += 1
         else:
@@ -46,50 +46,45 @@ try:
     )
     cursor = conn.cursor()
 
-    # Obtener los datos de la tabla de votantes
-    cursor.execute("SELECT dni, nombre, apellido, region, genero, edad, voto FROM votantes")
+    # Obtener el rango de edades
+    rango_edad_min = int(input("Ingrese el rango de edad mínimo: "))
+    rango_edad_max = int(input("Ingrese el rango de edad máximo: "))
+
+    # Filtrar los votantes dentro del rango de edad especificado
+    cursor.execute(f"SELECT dni, nombre, apellido, region, genero, edad, voto FROM votantes WHERE edad >= {rango_edad_min} AND edad <= {rango_edad_max}")
     resultados = cursor.fetchall()
 
-    # Guardar los datos en una lista
-    datos_votantes = []
-    for fila in resultados:
-        dni = fila[0]
-        nombre = fila[1]
-        apellido = fila[2]
-        region = fila[3]
-        genero = fila[4]
-        edad = fila[5]
-        voto = fila[6]
-        datos_votantes.append((dni, nombre, apellido, region, genero, edad, voto))
+    if resultados:
+        # Contar los votos por candidato
+        votos_por_candidato = {}
+        for fila in resultados:
+            candidato = fila[6]
+            if candidato in votos_por_candidato:
+                votos_por_candidato[candidato] += 1
+            else:
+                votos_por_candidato[candidato] = 1
+
+        # Obtener el candidato con más votos utilizando Merge Sort
+        resultados_ordenados = merge_sort(list(votos_por_candidato.items()))
+        candidato_mas_votos = resultados_ordenados[-1][0]
+        cantidad_votos = resultados_ordenados[-1][1]
+
+        # Obtener los detalles de los votos del candidato con más votos
+        cursor.execute(f"SELECT dni, nombre, apellido, region FROM votantes WHERE edad >= {rango_edad_min} AND edad <= {rango_edad_max} AND voto = '{candidato_mas_votos}'")
+        detalles_votos = cursor.fetchall()
+
+        print(f"Resultados para el rango de edad {rango_edad_min}-{rango_edad_max}:")
+        print(f"El candidato con más votos es {candidato_mas_votos} con {cantidad_votos} votos.")
+        print("Detalles de los votos:")
+        for fila in detalles_votos:
+            print(f"DNI: {fila[0]}, Nombre: {fila[1]}, Apellido: {fila[2]}, Región: {fila[3]}")
+
+    else:
+        print("No se encontraron votos para el rango de edad especificado.")
 
     # Cerrar el cursor y la conexión con la base de datos
     cursor.close()
     conn.close()
-
-    # Determinar el candidato con más votos utilizando Merge Sort
-    candidatos_ordenados = merge_sort(datos_votantes)
-
-    # print("Resultados de los votos:")
-    # for candidato in candidatos_ordenados:
-    #     print(f"Candidato {candidato[6]}: {candidato[1]} votos")
-
-    # Obtener los rangos de edad
-    edades = set(votante[5] for votante in datos_votantes)
-
-    # Filtrar y mostrar el candidato con más votos y la cantidad en cada rango de edad
-    for edad in sorted(edades):
-        votos_por_candidato = {}
-        for votante in datos_votantes:
-            if votante[5] == edad:
-                voto = votante[6]
-                if voto in votos_por_candidato:
-                    votos_por_candidato[voto] += 1
-                else:
-                    votos_por_candidato[voto] = 1
-
-        candidato_mas_votos = max(votos_por_candidato, key=votos_por_candidato.get)
-        cantidad_votos = votos_por_candidato[candidato_mas_votos]
-        print(f"Para el rango de edad {edad}, el candidato {candidato_mas_votos} tuvo {cantidad_votos} votos.")
 
 except mysql.connector.Error as error:
     print("Error al conectarse a la base de datos:", error)
