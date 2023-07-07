9f46760e689214c7f4ea049e43d6a64614eb3a1f
UserNanno <juan.canecillas@unmsm.edu.pe>
2023-07-07 18:45:31 -0500

    merge master

--- vista/admin.py
+++ vista/admin.py
@@ -4,6 +4,8 @@ from grafico_genero import GraficoGenero
 from grafico_edad import GraficoEdad
 from login import Login
 
+
+
 class Admin:
     def __init__(self):
         # Crear la ventana
@@ -41,9 +43,9 @@ class Admin:
         grafico=Grafico()
         grafico.crear_grafico()
 
-    # def boton2_click(self):
-    #     grafico2=GraficoRegion()
-    #     grafico2.crear_grafico()
+    def boton2_click(self):
+        grafico=Grafico()
+        grafico.crear_grafico()
         
     def boton3_click(self):
         grafico3=GraficoGenero()
