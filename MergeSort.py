2cd7668e58e7a2a36b67ef013ae7d04850047b58
UserNanno <juan.canecillas@unmsm.edu.pe>
2023-07-07 23:34:47 -0500

    cambiando ruta Mergesort.py

--- MergeSort.py
+++ MergeSort.py
@@ -1,6 +1,3 @@
-import mysql.connector
-
-# Función para realizar el ordenamiento Merge Sort
 def merge_sort(arr):
     if len(arr) <= 1:
         return arr
@@ -35,56 +32,3 @@ def merge(left, right):
         j += 1
 
     return result
-
-# Conectarse a la base de datos
-try:
-    conn = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='',
-        database='easyvote'
-    )
-    cursor = conn.cursor()
-
-    # Obtener el rango de edades
-    rango_edad_min = int(input("Ingrese el rango de edad mínimo: "))
-    rango_edad_max = int(input("Ingrese el rango de edad máximo: "))
-
-    # Filtrar los votantes dentro del rango de edad especificado
-    cursor.execute(f"SELECT dni, nombre, apellido, region, genero, edad, voto FROM votantes WHERE edad >= {rango_edad_min} AND edad <= {rango_edad_max}")
-    resultados = cursor.fetchall()
-
-    if resultados:
-        # Contar los votos por candidato
-        votos_por_candidato = {}
-        for fila in resultados:
-            candidato = fila[6]
-            if candidato in votos_por_candidato:
-                votos_por_candidato[candidato] += 1
-            else:
-                votos_por_candidato[candidato] = 1
-
-        # Obtener el candidato con más votos utilizando Merge Sort
-        resultados_ordenados = merge_sort(list(votos_por_candidato.items()))
-        candidato_mas_votos = resultados_ordenados[-1][0]
-        cantidad_votos = resultados_ordenados[-1][1]
-
-        # Obtener los detalles de los votos del candidato con más votos
-        cursor.execute(f"SELECT dni, nombre, apellido, region FROM votantes WHERE edad >= {rango_edad_min} AND edad <= {rango_edad_max} AND voto = '{candidato_mas_votos}'")
-        detalles_votos = cursor.fetchall()
-
-        print(f"Resultados para el rango de edad {rango_edad_min}-{rango_edad_max}:")
-        print(f"El candidato con más votos es {candidato_mas_votos} con {cantidad_votos} votos.")
-        print("Detalles de los votos:")
-        for fila in detalles_votos:
-            print(f"DNI: {fila[0]}, Nombre: {fila[1]}, Apellido: {fila[2]}, Región: {fila[3]}")
-
-    else:
-        print("No se encontraron votos para el rango de edad especificado.")
-
-    # Cerrar el cursor y la conexión con la base de datos
-    cursor.close()
-    conn.close()
-
-except mysql.connector.Error as error:
-    print("Error al conectarse a la base de datos:", error)
